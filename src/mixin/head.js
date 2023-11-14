ade06bca6d6a2c85a6ce94305c9c9b42ce55f0a9
IWABUCHI Yu(u)ki <butchiyu+com-github@gmail.com>
2023-11-14 19:58:15 +0900

    head mixinを汎用的に改修

--- src/mixin/head.js
+++ src/mixin/head.js
@@ -1,16 +1,21 @@
-const metaArr = [{
-    charset: "UTF-8"
-}, {
-    name: "viewport", content: "width=device-width, initial-scale=1.0"
-}]
+export default (propObj, ...slot) => {
+    const siteName = "butchi.jp"
 
-export default ({ ttlTxt }) => headTag`${[
-    titleTag`${ttlTxt}`,
-    linkTag`${{ rel: "stylesheet", href: "https://unpkg.com/material-components-web@latest/dist/material-components-web.min.css" }}`,
-    linkTag`${{ rel: "stylesheet", href: "https://fonts.googleapis.com/icon?family=Material+Icons" }}`,
-    linkTag`${{ rel: "stylesheet", href: "/css/style.css" }}`,
-    scriptTag`${{ type: "module", src: "/js/script.js" }}`,
-    scriptTag`${{ src: "https://unpkg.com/material-components-web@latest/dist/material-components-web.min.js" }}`,
+    const pageTtlTxt = propObj?.title
 
-    ...metaArr.map(m => metaTag`${m}`),
-]}`
+    const ttlTxt = pageTtlTxt ? `${pageTtlTxt} | ${siteName}` : siteName
+
+    return headTag`${[
+        metaTag`${{ charset: "UTF-8" }}`,
+        metaTag`${{ name: "viewport", content: "width=device-width, initial-scale=1.0" }}`,
+
+        titleTag`${ttlTxt}`,
+        linkTag`${{ rel: "stylesheet", href: "https://unpkg.com/material-components-web@latest/dist/material-components-web.min.css" }}`,
+        linkTag`${{ rel: "stylesheet", href: "https://fonts.googleapis.com/icon?family=Material+Icons" }}`,
+        linkTag`${{ rel: "stylesheet", href: "/css/style.css" }}`,
+        scriptTag`${{ type: "module", src: "/js/script.js" }}`,
+        scriptTag`${{ src: "https://unpkg.com/material-components-web@latest/dist/material-components-web.min.js" }}`,
+
+        ...slot
+    ]}`
+}
