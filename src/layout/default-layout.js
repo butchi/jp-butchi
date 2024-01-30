b03a8944717aea2e2826a8961b0e438374d644f0
IWABUCHI Yu(u)ki <butchiyu+com-github@gmail.com>
2024-01-30 16:38:28 +0900

    lint

--- src/layout/default-layout.js
+++ src/layout/default-layout.js
@@ -1,4 +1,15 @@
-const listDrawer = ({ href, iconName, label, tabIndex, isActivated, isCurrent }) => `<a class="mdc-deprecated-list-item${isActivated ? " mdc-deprecated-list-item--activated" : ''}" href="${href}"${isCurrent ? ' aria-current="page"' : ""} tabindex="${tabIndex}">
+const listDrawer = ({
+    href,
+    iconName,
+    label,
+    tabIndex,
+    isActivated,
+    isCurrent,
+}) => `<a class="mdc-deprecated-list-item${
+    isActivated ? " mdc-deprecated-list-item--activated" : ""
+}" href="${href}"${
+    isCurrent ? ' aria-current="page"' : ""
+} tabindex="${tabIndex}">
     <span class="mdc-deprecated-list-item__ripple"></span>
     <i class="material-icons mdc-deprecated-list-item__graphic" aria-hidden="true">${iconName}</i>
     <span class="mdc-deprecated-list-item__text">${label}</span>
@@ -36,10 +47,16 @@ const listItemArr = [
 
 const siteName = "butchi.jp"
 
-const defaultLayout = (_, propObj, ...slotArr) => `<aside class="mdc-drawer mdc-drawer--modal">
+const defaultLayout = (
+    _,
+    propObj,
+    ...slotArr
+) => `<aside class="mdc-drawer mdc-drawer--modal">
 <div class="mdc-drawer__content">
 <nav class="mdc-deprecated-list">
-${listItemArr.map((item, idx) => listDrawer({ ...item, tabIndex: idx })).join("")}
+${listItemArr
+    .map((item, idx) => listDrawer({ ...item, tabIndex: idx }))
+    .join("")}
 </nav>
 </div>
 </aside>
@@ -52,7 +69,9 @@ ${listItemArr.map((item, idx) => listDrawer({ ...item, tabIndex: idx })).join(""
             <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
                 <button class="material-icons mdc-top-app-bar__navigation-icon mdc-icon-button"
                     aria-label="Open navigation menu">menu</button>
-                <span class="mdc-top-app-bar__title">${propObj.title} | ${siteName}</span>
+                <span class="mdc-top-app-bar__title">${
+                    propObj.title
+                } | ${siteName}</span>
             </section>
             <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-end" role="toolbar">
                 <button class="material-icons mdc-top-app-bar__action-item mdc-icon-button"
@@ -67,7 +86,9 @@ ${listItemArr.map((item, idx) => listDrawer({ ...item, tabIndex: idx })).join(""
 </div>
 <div class="mdc-top-app-bar--fixed-adjust">
     <div class="mdc-layout-grid">
-        <div class="mdc-layout-grid__cell content">${slotArr[0].join("\n")}</div>
+        <div class="mdc-layout-grid__cell content">${slotArr[0].join(
+            "\n"
+        )}</div>
     </div>
 </div>
 <script>
