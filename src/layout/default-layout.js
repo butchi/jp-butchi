916530ff349c99a33745f29089ff004baead4f81
IWABUCHI Yu(u)ki <butchiyu+com-github@gmail.com>
2024-01-31 17:01:24 +0900

    デフォルトレイアウトをコンポーネントに合わせる

--- src/layout/default-layout.js
+++ src/layout/default-layout.js
@@ -1,19 +1,4 @@
-const listDrawer = ({
-    href,
-    iconName,
-    label,
-    tabIndex,
-    isActivated,
-    isCurrent,
-}) => `<a class="mdc-deprecated-list-item${
-    isActivated ? " mdc-deprecated-list-item--activated" : ""
-}" href="${href}"${
-    isCurrent ? ' aria-current="page"' : ""
-} tabindex="${tabIndex}">
-    <span class="mdc-deprecated-list-item__ripple"></span>
-    <i class="material-icons mdc-deprecated-list-item__graphic" aria-hidden="true">${iconName}</i>
-    <span class="mdc-deprecated-list-item__text">${label}</span>
-</a>`
+import "#root/src/lib/matra-vanilla.js"
 
 const listItemArr = [
     {
@@ -40,69 +25,41 @@ const listItemArr = [
     },
     {
         href: "/documents/",
-        iconName: "drafts",
+        iconName: "article",
         label: "解説記事",
     },
 ]
 
 const siteName = "butchi.jp"
 
-const defaultLayout = (
-    _,
-    propObj,
-    ...slotArr
-) => `<aside class="mdc-drawer mdc-drawer--modal">
-<div class="mdc-drawer__content">
-<nav class="mdc-deprecated-list">
-${listItemArr
-    .map((item, idx) => listDrawer({ ...item, tabIndex: idx }))
-    .join("")}
-</nav>
-</div>
-</aside>
-
-<div class="mdc-drawer-scrim"></div>
-
-<div class="mdc-drawer-app-content">
-    <header class="mdc-top-app-bar">
-        <div class="mdc-top-app-bar__row">
-            <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
-                <button class="material-icons mdc-top-app-bar__navigation-icon mdc-icon-button"
-                    aria-label="Open navigation menu">menu</button>
-                <span class="mdc-top-app-bar__title">${
-                    propObj.title
-                } | ${siteName}</span>
-            </section>
-            <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-end" role="toolbar">
-                <button class="material-icons mdc-top-app-bar__action-item mdc-icon-button"
-                    aria-label="Favorite">favorite</button>
-                <button class="material-icons mdc-top-app-bar__action-item mdc-icon-button"
-                    aria-label="Search">search</button>
-                <button class="material-icons mdc-top-app-bar__action-item mdc-icon-button"
-                    aria-label="Options">more_vert</button>
-            </section>
-        </div>
-    </header>
-</div>
-<div class="mdc-top-app-bar--fixed-adjust">
-    <div class="mdc-layout-grid">
-        <div class="mdc-layout-grid__cell content">${slotArr[0].join(
-            "\n"
-        )}</div>
-    </div>
-</div>
-<script>
-const MDCDrawer = mdc.drawer.MDCDrawer
-const MDCTopAppBar = mdc.topAppBar.MDCTopAppBar
-
-const drawer = MDCDrawer.attachTo(document.querySelector(".mdc-drawer"));
-
-const topAppBar = new MDCTopAppBar.attachTo(document.querySelector(".mdc-top-app-bar"))
-
-topAppBar.setScrollTarget(document.querySelector(".content"))
-topAppBar.listen('MDCTopAppBar:nav', _ => {
-    drawer.open = !drawer.open
-})
-</script>`
+const defaultLayout = (_, propObj, ...slotArr) =>
+    `
+        <btc-app-layout page-title="${propObj.title} | ${siteName}">
+            <ul slot="drawer-list">
+                ${listItemArr
+                    .map(item => {
+                        const {
+                            href,
+                            iconName,
+                            label,
+                            isActivated,
+                            isCurrent,
+                        } = item
+                        return liTag`
+                            ${Object.assign(
+                                {
+                                    "icon-name": iconName,
+                                },
+                                isActivated ? { "is-activated": true } : {},
+                                isCurrent ? { "is-current": true } : {}
+                            )}
+                            ${aTag`${{ href }}${[label]}`}
+                        `
+                    })
+                    .join("")}
+            </ul>
+            <div slot="content">${slotArr[0].join("")}</div>
+        </btc-app-layout>
+    `
 
 export default defaultLayout
